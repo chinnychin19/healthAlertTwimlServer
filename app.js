24fbc5185b7de8f757ad19afc41d1e065b141901
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-10-22 10:40:18 -0400

    adding texting support

--- app.js
+++ app.js
@@ -2,32 +2,22 @@ var express = require('express');
 var app = express();
 
 // queries: 
-// nameRecipient, 
-// nameConcerning, 
-// phoneNumber, 
+// recipientName, 
+// patientName, 
+// recipientPhoneNumber, 
+
 // statName, 
-// statValue, 
 // statUnit, 
+
+// statValue, 
 // statLowerBound, 
 // statUpperBound
-app.get('/message', function(req, res){
-    if (undefined == req.query.nameRecipient ||
-        undefined == req.query.nameConcerning ||
-        undefined == req.query.phoneNumber ||
-        undefined == req.query.statName ||
-        undefined == req.query.statValue ||
-        undefined == req.query.statUnit ||
-        undefined == req.query.statLowerBound ||
-        undefined == req.query.statUpperBound) {
+app.get('/callTwiml', function(req, res){
+    if (!validateQuery(req.query)) {
         res.status(400).send("You are missing a necessary parameter.");
     }
 
-    var message = '';
-    message += "Hello, "+req.query.nameRecipient+". ";
-    message += "This is a notification from Health Alert concering "+req.query.nameConcerning+". ";
-    message += "The patient's "+req.query.statName + " was measured at " + req.query.statValue + " " + req.query.statUnit + ". ";
-    message += "This falls outside of the prescribed range between " + req.query.statLowerBound + " and "+ req.query.statUpperBound + ". ";
-    message += "You are receiving this notification because " +req.query.nameConcerning + " indicated you as an emergency contact.";
+    var message = getMessageText(req.query);
 
     var output = '<?xml version="1.0" encoding="UTF-8"?>' +
         '<Response>' +
@@ -38,26 +28,19 @@ app.get('/message', function(req, res){
     console.log(output);
 });
 
-app.get('/notify', function(req, res) {
+app.get('/notifyWithCall', function(req, res) {
     var accountSid = process.env.TWILIO_SID;
     var authToken = process.env.TWILIO_AUTH_TOKEN;
     var twilioNumber = process.env.TWILIO_PHONE_NUMBER;
     var client = require('twilio')(accountSid, authToken);
 
-    if (undefined == req.query.nameRecipient ||
-        undefined == req.query.nameConcerning ||
-        undefined == req.query.phoneNumber ||
-        undefined == req.query.statName ||
-        undefined == req.query.statValue ||
-        undefined == req.query.statUnit ||
-        undefined == req.query.statLowerBound ||
-        undefined == req.query.statUpperBound) {
+    if (!validateQuery(req.query)) {
         res.status(400).send("You are missing a necessary parameter.");
     }
 
     console.log(req.query);
 
-    var messageUrl = "http://dukecs408-twilio.herokuapp.com/message?";
+    var messageUrl = "http://dukecs408-twilio.herokuapp.com/callTwiml?";
     for (var q in req.query) {
         messageUrl += q + "=" + req.query[q] + "&";
     }
@@ -67,7 +50,7 @@ app.get('/notify', function(req, res) {
 
     client.calls.create({
         url: messageUrl,
-        to: req.query.phoneNumber,
+        to: req.query.recipientPhoneNumber,
         from: twilioNumber,
         method: "GET"
     }, function(err, call) {
@@ -80,9 +63,60 @@ app.get('/notify', function(req, res) {
     });
 });
 
+app.get('/notifyWithText', function(req, res) {
+    var accountSid = process.env.TWILIO_SID;
+    var authToken = process.env.TWILIO_AUTH_TOKEN;
+    var twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+    var client = require('twilio')(accountSid, authToken);
+
+    if (!validateQuery(req.query)) {
+        res.status(400).send("You are missing a necessary parameter.");
+    }
+
+    console.log(req.query);
+
+    client.messages.create({
+        to: req.query.recipientPhoneNumber,
+        from: twilioNumber,
+        body: getMessageText(req.query)
+    }, function(err, call) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.send("sent");
+        return;
+    });
+});
+
+function getMessageText(query) {
+    var message = '';
+    message += "Hello, "+query.recipientName+". ";
+    message += "This is a notification from Health Alert concering "+query.patientName+". ";
+    message += "The patient's "+query.statName + " was measured at " + query.statValue + " " + query.statUnit + ". ";
+    message += "This falls outside of the prescribed range between " + query.statLowerBound + " and "+ query.statUpperBound + ". ";
+    message += "You are receiving this notification because " +query.patientName + " indicated you as an emergency contact.";
+    return message;
+}
+
+function validateQuery(query) {
+    if (undefined == query.recipientName ||
+        undefined == query.patientName ||
+        undefined == query.recipientPhoneNumber ||
+        undefined == query.statName ||
+        undefined == query.statValue ||
+        undefined == query.statUnit ||
+        undefined == query.statLowerBound ||
+        undefined == query.statUpperBound) {
+        return false;
+    }
+    return true;
+}
+
 // test URL:
-// http://localhost:3000/notify?nameRecipient=chinmay&nameConcerning=jeff daye&phoneNumber=7034855298&statName=power level&statValue=9001&statUnit=units&statLowerBound=0&statUpperBound=9000
-// http://dukecs408-twilio.herokuapp.com/notify?nameRecipient=chinmay&nameConcerning=jeff%20daye&phoneNumber=7034855298&statName=power%20level&statValue=9001&statUnit=units&statLowerBound=0&statUpperBound=9000
+// http://localhost:3000/notifyWithCall?recipientName=chinmay&patientName=jeff daye&recipientPhoneNumber=7034855298&statName=power level&statValue=9001&statUnit=units&statLowerBound=0&statUpperBound=9000
+// http://localhost:3000/notifyWithText?recipientName=chinmay&patientName=jeff daye&recipientPhoneNumber=7034855298&statName=power level&statValue=9001&statUnit=units&statLowerBound=0&statUpperBound=9000
+// http://dukecs408-twilio.herokuapp.com/notifyWithCall?recipientName=chinmay&patientName=jeff%20daye&recipientPhoneNumber=7034855298&statName=power%20level&statValue=9001&statUnit=units&statLowerBound=0&statUpperBound=9000
 
 
 
